6ba37ddd25eb6e698ecba261675a61b0f90e3646
Rahul <rb4724203@gmail.com>
2026-05-06 21:25:29 +0530

    Upload full portfolio project

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   base: '/rahul-portfolio/',
   server: {
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
     
   },
+  
   preview: {
     host: '0.0.0.0',
     port: 3000,
